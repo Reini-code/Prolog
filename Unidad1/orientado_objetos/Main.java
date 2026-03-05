38d1d2024d2fbf25bf9d5e84f1209fcff9eb65bc
Reini-code <rcf.azteca@gmail.com>
2026-03-05 08:11:30 -0600

    mod clase 1

--- Unidad1/orientado_objetos/Main.java
+++ Unidad1/orientado_objetos/Main.java
@@ -1,13 +1,4 @@
-class Persona {
-
-    String nombre;
-    int edad;
-
-    void saludar(){
-        System.out.println("Hola, mi nombre es " + nombre + " y tengo " + edad + " años.");
-    }
-
-}
+package Unidad1.orientado_objetos;
 
 public class Main {
 
---
+++ Unidad1/orientado_objetos/Persona.java
@@ -0,0 +1,12 @@
+package Unidad1.orientado_objetos;
+
+class Persona {
+
+    String nombre;
+    int edad;
+
+    void saludar(){
+        System.out.println("Hola, mi nombre es " + nombre + " y tengo " + edad + " años.");
+    }
+
+}
